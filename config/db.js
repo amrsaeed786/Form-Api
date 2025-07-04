1c1c263a30100bc88f4014a46f2eb0e78a16d2b0
Amir Saeed <hafizamirsaeed082@gmail.com>
2025-07-04 22:06:01 +0500

    db

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ dotenv.config({ silent: true });
 const connectdB = async () => {
   try {
     if (mongoose.connection.readyState >= 1) return; // avoid reconnecting
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(`${process.env.MONGO_URL}`);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
